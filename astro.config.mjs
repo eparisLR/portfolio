778ba2786e440dd7389832ee8a383c3d91ea6bfd
Nebich <paris.emilien@hotmail.fr>
2024-07-06 14:34:53 +0200

    🚀 Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://portfolio.grosbi.de",
-  integrations: [preact()]
+  integrations: [preact()],
+  output: 'server',
+  adapter: node({
+    mode: "standalone"
+  })
 });
