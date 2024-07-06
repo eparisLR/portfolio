cdabf11f65e16dbd07f5baf7757f595971c64127
Nebich <paris.emilien@hotmail.fr>
2024-07-06 21:38:51 +0200

    ✨ Adding storybook

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
 import node from "@astrojs/node";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://portfolio.grosbi.de",
-  integrations: [preact()],
+  integrations: [react()],
   output: 'server',
   adapter: node({
     mode: "standalone"
